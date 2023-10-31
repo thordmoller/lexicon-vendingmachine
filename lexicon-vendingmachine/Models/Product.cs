395eb24f919d8b5d0486abc828b9dc274c9db151
Thord Möller <thord_moller_92@hotmail.com>
2023-10-31 05:43:42 +0100

    made the abstract product class

---
+++ lexicon-vendingmachine/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lexicon_vendingmachine
+{
+    public abstract class Product
+    {
+
+        public string? Name { get; protected set; }
+        public decimal Price { get; protected set; }
+
+        public abstract void Examine();
+        public abstract void Use();
+    }
+}
