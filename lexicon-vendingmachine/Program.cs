de27fe78e66230af3687be85a935a539cb3ec8ed
Thord Möller <thord_moller_92@hotmail.com>
2023-10-31 02:59:47 +0100

    Add project files.

---
+++ lexicon-vendingmachine/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace lexicon_vendingmachine
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
